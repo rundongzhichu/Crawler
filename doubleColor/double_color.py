7c89b47e07920b56063363dca12b7934c956fa63
sunshoucai <17396248231@163.com>
2025-06-29 19:22:24 +0800

    拉取福利彩票

--- doubleColor/double_color.py
+++ doubleColor/double_color.py
@@ -9,11 +9,11 @@ import urllib3
 from fake_useragent import UserAgent
 from pathlib import Path
 
-from openpyxl import Workbook, load_workbook
-
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-types=['ssq','kl8','3d','qlc']
+# types=['ssq','kl8','3d','qlc']
+
+types=['kl8','3d','qlc']
 sleep_time=2
 
 def processSsq(type:str, data:list):
@@ -43,23 +43,25 @@ def processSsq(type:str, data:list):
     if not file_path.exists():
         df.to_excel(file_name, sheet_name=sheet_name, header=True, index=False)
     else:
-        # 追加到现有 Excel 文件
-        with pd.ExcelWriter(file_name, engine='openpyxl') as writer:
-            # 1. 加载现有 Excel 文件
-            book = load_workbook(file_path)
-            writer.book = book  # 绑定已加载的工作簿
-            # 2. 获取当前 Sheet 的最后一行
 
+        # 追加到现有 Excel 文件
+        with pd.ExcelWriter(file_name, engine='openpyxl', mode='a', if_sheet_exists='overlay') as writer:
+            # 1. 加载现有 Excel 文件
+            book = writer.book
             start_row = 0
             if sheet_name in book.sheetnames:
                 start_row = book[sheet_name].max_row  # 最后一行行号
             else:
+                book.create_sheet(sheet_name)
                 start_row = 0  # 如果 Sheet 不存在，从第 1 行开始
+
+            sheet = book[sheet_name]
             # 3. 追加数据（跳过列名 header=False）
             df.to_excel(
                 writer,
                 sheet_name=sheet_name,
-                startrow=start_row if start_row == 0 else start_row + 1,  # 避免重复列名
+                # startrow=start_row if start_row == 0 else start_row + 1,  # 避免重复列名
+                startrow=start_row,
                 header=False if start_row > 0 else True,  # 只有第一行写列名
                 index=False
             )
@@ -112,18 +114,17 @@ for type in types:
     # 分页获取数据
     for i in range(1, int(total_cnt/page_size + 1)):
         # 参数名字
-        type = 'ssq'
         page_no = i
         url = f'https://www.cwl.gov.cn/cwl_admin/front/cwlkj/search/kjxx/findDrawNotice?name={type}&issueCount=&issueStart=&issueEnd=&dayStart=&dayEnd=&pageNo={i}&pageSize={page_size}&week=&systemType=PC'
         time.sleep(sleep_time)
-        # try:
-        r = session.get(url, headers=headers, timeout=10)
-        r.encoding = chartset.detect(r.content)['encoding']
-        content = r.content.decode('utf-8')
-        if(content != '' and content != None):
-            data = json.loads(content)
-            res = data['result']
-            processSsq(type, res)
-            print(f"success process page {page_no} of {int(total_cnt / page_size + 1)}，pagesize：{page_size}， total:{total_cnt}")
-        # except Exception as e:
-        #     print(f"ERROR: process page {page_no} of {int(total_cnt/page_size + 1)}，pagesize：{page_size}， total:{total_cnt}", e)
+        try:
+            r = session.get(url, headers=headers, timeout=10)
+            r.encoding = chartset.detect(r.content)['encoding']
+            content = r.content.decode('utf-8')
+            if(content != '' and content != None):
+                data = json.loads(content)
+                res = data['result']
+                processSsq(type, res)
+                print(f"success process type {type} page {page_no} of {int(total_cnt / page_size + 1)}，pagesize：{page_size}， total:{total_cnt}")
+        except Exception as e:
+            print(f"ERROR: process type {type} page {page_no} of {int(total_cnt/page_size + 1)}，pagesize：{page_size}， total:{total_cnt}", e)
